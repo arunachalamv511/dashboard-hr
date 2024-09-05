0fd7e05d9088a508165fd09907c6c57f5ad09c52
Arunachalam511 <arunachalam.v@eminds.ai>
2024-09-05 18:11:53 +0530

    ping api added

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 const port = 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 // For any route that doesn’t match a static file, send the React app’s index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/api/ping", (req, res) => {
+  res.status(200).send("success");
 });
 
 app.listen(port, () => {
