d1ba705c0a07ced9048f20aba9f0caf45cc8815f
Arunachalam511 <arunachalam.v@eminds.ai>
2024-09-05 18:06:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// For any route that doesn’t match a static file, send the React app’s index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
